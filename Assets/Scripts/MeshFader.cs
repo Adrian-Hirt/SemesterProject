78da23c918ea8d2ec69c71893242bc3b7cc2f32d
Adrian Hirt <aedu.hirt@gmail.com>
2023-01-19 17:38:16 +0100

    Fix some bugs with navigation

--- Assets/Scripts/MeshFader.cs
+++ Assets/Scripts/MeshFader.cs
@@ -11,7 +11,6 @@ public class MeshFader : MonoBehaviour {
   public void Start() {
     // Reset shader to the invisible shader
     material.shader = invisibleShader;
-    //material.shader = diffuseShader;
   }
 
   public void ToggleEnvironmentShaders() {
@@ -27,53 +26,4 @@ public class MeshFader : MonoBehaviour {
     }
 
   }
-
-  //public void UpdateAlphaFromSlider(SliderEventData eventData) {
-  //  return;
-  //  if (eventData.NewValue < 0.01f) {
-  //    // Switch to invisible shader to completely hide the mesh
-  //    material.shader = invisibleShader;
-
-  //  }
-  //  else if (eventData.NewValue > 0.99f) {
-  //    // Switch to standard diffuse shader
-  //    material.shader = diffuseShader;
-
-  //    // Enable opaque mode
-  //    material.SetFloat("_Mode", 0); // opaque
-  //    material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-  //    material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-  //    material.SetInt("_ZWrite", 1);
-  //    material.DisableKeyword("_ALPHATEST_ON");
-  //    material.DisableKeyword("_ALPHABLEND_ON");
-  //    material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-  //    material.renderQueue = -1;
-
-  //    // Update the color
-  //    material.color = diffuseInitialColor;
-  //  }
-  //  else {
-  //    // Switch to standard diffuse shader
-  //    material.shader = diffuseShader;
-
-  //    // Enable transparent mode
-  //    material.SetFloat("_Mode", 3); // transparent
-  //    material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-  //    material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-  //    material.SetInt("_ZWrite", 0);
-  //    material.DisableKeyword("_ALPHATEST_ON");
-  //    material.DisableKeyword("_ALPHABLEND_ON");
-  //    material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
-  //    material.renderQueue = 3000;
-
-  //    float colorValue = eventData.NewValue * colorStepFactor;
-
-  //    // Get color from material
-  //    Color color = new Color(colorValue, colorValue, colorValue, eventData.NewValue);
-
-  //    // Update the color
-  //    material.color = color;
-  //  }
-  //}
-
 }
--- Assets/Scripts/MoveCameraToKnownPosition.cs
+++ Assets/Scripts/MoveCameraToKnownPosition.cs
@@ -5,25 +5,14 @@ public class MoveCameraToKnownPosition : MonoBehaviour {
   public GameObject sceneContent;
 
   public void SetPositionOfCamera(GameObject target) {
-    // First we reset the playspace to the zero point
-    //Vector3 resetPlayspace = Vector3.zero - MixedRealityPlayspace.Transform.position;
-    //MixedRealityPlayspace.Transform.Translate(resetPlayspace);
+    // First we reset the playspace to the zero point, such that it's
+    // easier to compute the other translations below
+    MixedRealityPlayspace.Position = Vector3.zero;
 
-    //// Compute the transform to the new position
-    //Vector3 playspaceOffset = target.transform.position; // - Camera.main.transform.position;
-    ////playspaceOffset.z += 1.7f;
+    // Compute the new position we'll need to set
+    Vector3 targetTranslate = target.transform.position - Camera.main.transform.position;
 
-    //// Update the playspace position
-    //MixedRealityPlayspace.Transform.Translate(playspaceOffset);
-
-    // Set the position of the playspace to the chosen target
-    //Vector3 targetTranslate =  target.transform.position - MixedRealityPlayspace.Transform.position;
-    //MixedRealityPlayspace.Transform.Translate(targetTranslate, Space.World);
-
-    // Reset position
-    sceneContent.transform.position = Vector3.zero;
-
-    // Update position of scene content
-    sceneContent.transform.position = (-1 * target.transform.position) + Camera.main.transform.position;
+    // Update position of playspace
+    MixedRealityPlayspace.Position = targetTranslate;
   }
 }
